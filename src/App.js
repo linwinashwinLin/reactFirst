2bb58c282595e45e1aae31c1cc9ae8db9de1baa7
ashwin-14876 <ashwin.madhu@zohocorp.com>
2024-10-17 13:21:16 +0530

    Content Change

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="card">
         <div className="texts">
-          <h1>A PROUD EMPLOYEE</h1>
-          <p>I am working here since the very beginning.. A PROUD EMPLOYEE</p>
+          <h1>A PROUD TEAM</h1>
+          <p>Together from the very start, we’ve built something special… A PROUD TEAM, united in purpose!</p>
         </div>
         <img src="https://blogimage.vantagecircle.com/content/images/2024/01/Employee-Experience-Everything-You-Need-To-Know-in-2024-Featured-Image.png" alt="An Error Occured!"/>
       </div>
