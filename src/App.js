bbd1ea9a264faf2fb85e784e3d40f148e677cae0
ashwin-14876 <ashwin.madhu@zohocorp.com>
2024-10-17 13:12:42 +0530

    First react work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="card">
+        <div className="texts">
+          <h1>A PROUD EMPLOYEE</h1>
+          <p>I am working here since the very beginning.. A PROUD EMPLOYEE</p>
+        </div>
+        <img src="https://blogimage.vantagecircle.com/content/images/2024/01/Employee-Experience-Everything-You-Need-To-Know-in-2024-Featured-Image.png" alt="An Error Occured!"/>
+      </div>
   );
 }
 
